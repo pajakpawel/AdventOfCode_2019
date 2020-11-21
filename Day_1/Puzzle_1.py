de2ec459ef991631dd9a6d26fd2cf26e7d524f18
pajakpawel <pajakpawel@vp.pl>
2020-11-21 21:56:06 +0100

    Updated <calculate_fuel_required> function to calculate and return the required amout of fuel

--- Day_1/Puzzle_1.py
+++ Day_1/Puzzle_1.py
@@ -1,6 +1,11 @@
-def calculate_fuel_required(dataset):
-    pass
+def calculate_fuel_required(modules_masses: list) -> int:
+    fuel_required = 0
+
+    for mass in modules_masses:
+        fuel_required += (mass // 3) - 2
+
+    return fuel_required
 
 
 if __name__ == '__main__':
-    calculate_fuel_required(10)
+    calculate_fuel_required([10])

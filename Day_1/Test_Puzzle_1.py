c36d934a439db8e45d0f21827bf0fb9d999a2e80
pajakpawel <pajakpawel@vp.pl>
2020-11-21 21:12:57 +0100

    Updated test to check correctness of fuel calculation

--- Day_1/Test_Puzzle_1.py
+++ Day_1/Test_Puzzle_1.py
@@ -1,9 +1,23 @@
 import unittest
+from Day_1 import Puzzle_1
 
 
 class TestFirstPuzzle(unittest.TestCase):
     def test_count_fuel_required(self):
-        self.assertEqual(True, False)
+
+        datasets_with_solutions = (
+            # A tuple of (dataset, solution)
+            (12, 2),
+            (14, 2),
+            (1969, 654),
+            (100756, 33583),
+            ({12, 14}, 4),
+            ({12, 14, 1969}, 658)
+        )
+
+        for dataset, solution in datasets_with_solutions:
+            with self.subTest("Solution correctness for given dataset", dataset=dataset, solution=solution):
+                self.assertEqual(Puzzle_1.count_fuel_required(dataset), solution)
 
 
 if __name__ == '__main__':

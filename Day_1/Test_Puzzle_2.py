89494c3a6a42ef7855892bfeff627816b221cb19
pajakpawel <pajakpawel@vp.pl>
2020-11-30 23:58:05 +0100

    Added unit test for recursive_fuel_calculation function, which will recursively calculate fuel required for given mass

--- Day_1/Test_Puzzle_2.py
+++ Day_1/Test_Puzzle_2.py
@@ -3,6 +3,20 @@ from Day_1 import Puzzle_2
 
 
 class TestSecondPuzzle(unittest.TestCase):
+    def test_recursive_fuel_calculate(self):
+
+        masses_with_solutions = (
+            # A tuple of (mass, solution)
+            (1, 0),
+            (14, 2),
+            (1969, 966),
+            (100756, 50346),
+        )
+
+        for mass, solution in masses_with_solutions:
+            with self.subTest("Recursive fuel correctness for given dataset", dataset=mass, solution=solution):
+                self.assertEqual(Puzzle_2.recursive_fuel_calculate(mass), solution)
+
     def test_calculate_entire_fuel_required(self):
 
         datasets_with_solutions = (
@@ -16,7 +30,7 @@ class TestSecondPuzzle(unittest.TestCase):
         )
 
         for dataset, solution in datasets_with_solutions:
-            with self.subTest("Solution correctness for given dataset", dataset=dataset, solution=solution):
+            with self.subTest("Entire fuel correctness for given dataset", dataset=dataset, solution=solution):
                 self.assertEqual(Puzzle_2.calculate_entire_fuel_required(dataset), solution)
 
 
